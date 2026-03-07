fde9018227f399178cb745b5aca9948d45c8c893
ryotairi <ryotairi.383e@extera.xyz>
2026-03-07 13:40:21 +0500

    shorter player IDs

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -2,7 +2,7 @@ import { randomInt } from "crypto";
 
 export function generateUserId(): number {
     var str = '';
-    for (let i = 0; i < 18; i++) {
+    for (let i = 0; i < 14; i++) {
         str += randomInt(10).toString();
     }
     return parseInt(str);
