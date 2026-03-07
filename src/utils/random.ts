6317f2bc25821b4990947eabae681bbbf3f22054
ryotairi <ryotairi.383e@extera.xyz>
2026-03-07 11:50:09 +0500

    source maps and reduce userid length

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -2,7 +2,7 @@ import { randomInt } from "crypto";
 
 export function generateUserId(): number | bigint {
     var str = '';
-    for (let i = 0; i < 24; i++) {
+    for (let i = 0; i < 12; i++) {
         str += randomInt(10).toString();
     }
     return BigInt(str);
